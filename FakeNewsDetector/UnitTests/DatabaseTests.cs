58657934a7902551f4f7ca3901e4f78f1eeb427a
Cristian Bucataru <cristian.bucataru@gmail.com>
2021-11-08 12:42:48 +0200

    Unit tests, Frontend, API

---
+++ FakeNewsDetector/UnitTests/DatabaseTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class DatabaseTests
+    {
+        [Test]
+        public void Send_Database_Query()
+        {
+            Assert.True(true);
+        }
+    }
+}
